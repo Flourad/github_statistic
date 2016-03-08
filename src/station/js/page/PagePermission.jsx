a0699347ee5faf3690ee30d923bf9ceb9395efe0
jinjiaxing <jinjiaxing@MacBook-Pro.local>
2016-03-08 18:01:26 +0800

    update bduss
    Change-Id: Ifb571c305896529fde998130ad80ddd40e5636fe

--- src/station/js/page/PagePermission.jsx
+++ src/station/js/page/PagePermission.jsx
@@ -6,87 +6,76 @@ let { Button, Icon, Table } =AntD;
 class PagePermission extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            dataSource:this.data
-        };
-
-        this.columns = [
-            {
-                title: 'id',
-                dataIndex: 'id',
-                key: 'id',
-                render(text) {
-                    return <div style={{display:'none'}}></div>;
-                }
-            },
-
-            {
-            title: '姓名',
-            dataIndex: 'name',
-            key: 'name',
-            render(text) {
-                return <a href="#">{text}</a>;
-            }
-        }, {
-            title: '手机号',
-            dataIndex: 'age',
-            key: 'age',
-        }, {
-            title: '管辖油站',
-            dataIndex: 'address',
-            key: 'address',
-        }, {
-            title: '操作',
-            key: 'operation',
-            render(text, record) {
-                return (
-                    <span>
-                    <a href="#">编辑{record.name}</a>
-                    <span className="ant-divider"></span>
-                    <a href="#">删除</a>
-                    <span className="ant-divider"></span>
-                  </span>
-                );
-            }
-        }];
 
         this.data = [{
             key: '1',
             name: '胡彦斌',
-            age: 32,
-            address: '西湖区湖底公园1号'
+            phone: '13904015882',
+            address: '北京西湖区湖底公园1号'
         }, {
             key: '2',
             name: '胡彦祖',
-            age: 42,
-            address: '西湖区湖底公园1号'
+            phone: '13905015882',
+            address: '天津西湖区湖底公园2号'
         }, {
             key: '3',
             name: '李大嘴',
-            age: 32,
-            address: '西湖区湖底公园1号'
+            phone: '13914015782',
+            address: '沈阳西湖区湖底公园3号'
         }];
 
+        this.state = {
+            dataSource: this.data
+        };
+
+        this.columns = [
+            {
+                title: '姓名',
+                dataIndex: 'name',
+                key: 'name',
+                render(text) {
+                    return <a href="#">{text}</a>;
+                }
+            }, {
+                title: '手机号',
+                dataIndex: 'phone',
+                key: 'phone',
+            }, {
+                title: '管辖油站',
+                dataIndex: 'address',
+                key: 'address',
+            }, {
+                title: '操作',
+                key: 'operation',
+                render(text, record) {
+                    return (
+                        <div>
+                            <button className="columnButton">编辑</button>
+                            <button className="columnButton">删除</button>
+                        </div>
+                    );
+                }
+            }];
     }
 
-    componentDidMount(){
+    componentDidMount() {
 
     }
 
-    componentWillMount(){
-        var me=this;
-        $.ajax({
-            type: 'get',
-            data:'',
-            url: '/admin/operator/list',
-            dataType:'json',
-            success: function(data){
-                console.log('+++++++++++++++++');
-                console.log(data);
-                me.setState({dataSource:data.data.operator});
-            }
-
-        });
+    componentWillMount() {
+        //var me=this;
+        //$.ajax({
+        //    type: 'get',
+        //    data:'',
+        //    url: '/admin/account/info',
+        //    dataType:'json',
+        //    success: function(data){
+        //        console.log('+++++++++++++++++');
+        //        console.log(data);
+        //        me.setState({dataSource:data.data.operator});
+        //    }
+        //
+        //});
     }
 
     render() {
@@ -101,6 +90,7 @@ class PagePermission extends React.Component {
                 </div>
                 <div className="pagePermission_Table">
                     <Table columns={this.columns} dataSource={this.state.dataSource}/>
+                    <label className="dataTotal">共 3 条记录</label>
                 </div>
             </div>
         );
