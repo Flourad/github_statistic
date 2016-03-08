e006f8f10668939b4b8aa123cf64cfe79abb7b8f
jinjiaxing <jinjiaxing@MacBook-Pro.local>
2016-03-08 16:21:47 +0800

    add pagePermission
    Change-Id: Iaebfbd9b1d21b5ea2b5d3e988c2e60215d8f5842

--- src/station/js/page/PagePermission.jsx
+++ src/station/js/page/PagePermission.jsx
@@ -1,28 +1,111 @@
 /**
  * Created by jinjiaxing on 16/3/4.
  */
+let { Button, Icon, Table } =AntD;
+
 class PagePermission extends React.Component {
     constructor(props) {
         super(props);
-        this.state={
-
+        this.state = {
+            dataSource:this.data
         };
+
+        this.columns = [
+            {
+                title: 'id',
+                dataIndex: 'id',
+                key: 'id',
+                render(text) {
+                    return <div style={{display:'none'}}></div>;
+                }
+            },
+
+            {
+            title: '姓名',
+            dataIndex: 'name',
+            key: 'name',
+            render(text) {
+                return <a href="#">{text}</a>;
+            }
+        }, {
+            title: '手机号',
+            dataIndex: 'age',
+            key: 'age',
+        }, {
+            title: '管辖油站',
+            dataIndex: 'address',
+            key: 'address',
+        }, {
+            title: '操作',
+            key: 'operation',
+            render(text, record) {
+                return (
+                    <span>
+                    <a href="#">编辑{record.name}</a>
+                    <span className="ant-divider"></span>
+                    <a href="#">删除</a>
+                    <span className="ant-divider"></span>
+                  </span>
+                );
+            }
+        }];
+
+        this.data = [{
+            key: '1',
+            name: '胡彦斌',
+            age: 32,
+            address: '西湖区湖底公园1号'
+        }, {
+            key: '2',
+            name: '胡彦祖',
+            age: 42,
+            address: '西湖区湖底公园1号'
+        }, {
+            key: '3',
+            name: '李大嘴',
+            age: 32,
+            address: '西湖区湖底公园1号'
+        }];
+
     }
 
-    render(){
-        return(
-            <div id="PagePermission">
+    componentDidMount(){
+
+    }
+
+    componentWillMount(){
+        var me=this;
+        $.ajax({
+            type: 'get',
+            data:'',
+            url: '/admin/operator/list',
+            dataType:'json',
+            success: function(data){
+                console.log('+++++++++++++++++');
+                console.log(data);
+                me.setState({dataSource:data.data.operator});
+            }
+
+        });
+    }
+
+    render() {
+        return (
+            <div id="pagePermission">
                 <div className="pagePermission_Header">
-                    <h1>权限管理</h1>
-                    <button> 新增人员 </button>
+                    <h1 className="headtext">权限管理</h1>
+                    <Button className="headButton" type="primary" size="large">
+                        <Icon type="plus"/>
+                        新增人员
+                    </Button>
                 </div>
                 <div className="pagePermission_Table">
-                    12121
+                    <Table columns={this.columns} dataSource={this.state.dataSource}/>
                 </div>
             </div>
         );
     }
 }
 
-PagePermission.defaultProps={};
+PagePermission.defaultProps = {};
 export default PagePermission;
